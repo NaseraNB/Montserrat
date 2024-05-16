16e4f42c47dc6b2b20a82a6a6df4696734f4f6c7
NaseraNB <04nasera.b@inscastellet.cat>
2024-05-16 19:08:36 +0200

    Creació de la carpeta models i la classe Inici de sessió

---
+++ app/src/main/java/com/example/cremallerademontserrat/models/Inici_De_Sessio.kt
@@ -0,0 +1,12 @@
+package com.example.cremallerademontserrat.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cremallerademontserrat.R
+
+class Inici_De_Sessio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inici_de_sessio)
+    }
+}
